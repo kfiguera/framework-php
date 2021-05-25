4fc1504c3ec1bc11d684c6487c3a35c361a3a20e
Kevin Figuera <kevinalejandro3@gmail.com>
2021-05-24 23:46:51 -0400

    creacion de la clase reponse para su configuración

---
+++ app/Http/Response.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    /**
+     * @var $view
+     */
+    protected $view; //array, json, pdf
+
+    /**
+     * Response constructor.
+     * @param $view
+     */
+    public function __construct($view)
+    {
+        $this->view = $view;// home, contact
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    public function send()
+    {
+        $view = $this->getView();
+        $content = file_get_contents(__DIR__."../../views/$view.php");
+
+        require __DIR__."../../views/layout.php";
+    }
+
+}
